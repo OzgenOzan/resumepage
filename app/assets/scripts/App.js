0b5830eda0743a6e9a5238b58d1f8d12913dbfcd
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2021-12-20 23:28:43 +0300

    added modal window

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import "../styles/styles.css";
 import MobileMenu from "./Modules/MobileMenu.js";
+import Modal from "./Modules/Modal";
 
+new Modal();
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
