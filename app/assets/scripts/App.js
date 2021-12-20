033a3fe4825a065911fac15dd8a6ce82e30ce154
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2021-12-20 19:42:02 +0300

    ...

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
-"use strict";
-
 import "../styles/styles.css";
+import MobileMenu from "./Modules/MobileMenu.js";
+
+let mobileMenu = new MobileMenu();
+
 if (module.hot) {
   module.hot.accept();
 }
-
-/* Lesson trial */
