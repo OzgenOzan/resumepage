d5535a1dbf31f2bcb24ddfbac6024efd2ae94b0b
OzgenOzan <202050014003@ogr.akdeniz.edu.tr>
2021-12-23 20:05:42 +0300

    final commit

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,8 @@ const postCSSPlugins = [
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap("Copy images", function () {
-      fse.copySync("./app/assets/images", "./dist/assets/images");
-      fse.copySync("./app/assets/documents", "./dist/assets/documents");
+      fse.copySync("./app/assets/images", "./docs/assets/images");
+      fse.copySync("./app/assets/documents", "./docs/assets/documents");
     });
   }
 }
@@ -79,7 +79,7 @@ if (currentTask == "build") {
   config.output = {
     filename: "[name].[chunkhash].js",
     chunkFilename: "[name].[chunkhash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   };
   config.mode = "production";
   config.optimization = {
